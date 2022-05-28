8bd1ef49e546a343242611226bc49eb159145ab0
Jonny Derenge <Jderenge01@gmail.com>
2022-05-28 00:40:46 -0500

    Completed part 2

--- day03/day03.py
+++ day03/day03.py
@@ -4,20 +4,38 @@ from enum import unique
 f = open("day03/day03_input.txt")
 
 lines = f.readlines()
-coord = [(0,0)]
+
+coord =  [(0,0)]
+rcoord = [(0,0)]
 
 for line in lines:
     counter = 0
     for character in line:
         if character == '<':
-            coord.append((coord[-1][0] -1, coord[-1][1]))
+            # coord.append((coord[-1][0] -1, coord[-1][1]))
+            if counter % 2 == 0:
+                coord.append((coord[-1][0] -1, coord[-1][1]))
+            else:
+                rcoord.append((rcoord[-1][0] -1, rcoord[-1][1]))
         elif character == '>':
-            coord.append((coord[-1][0] +1, coord[-1][1]))
+            # coord.append((coord[-1][0] +1, coord[-1][1]))
+            if counter % 2 == 0:
+                coord.append((coord[-1][0] +1, coord[-1][1]))
+            else:
+                rcoord.append((rcoord[-1][0] +1, rcoord[-1][1]))
         elif character == '^':
-            coord.append((coord[-1][0], coord[-1][1] +1))
+            # coord.append((coord[-1][0], coord[-1][1] +1))
+            if counter % 2 == 0:
+                coord.append((coord[-1][0], coord[-1][1] +1))
+            else:
+                rcoord.append((rcoord[-1][0], rcoord[-1][1] +1))
         else:
-            coord.append((coord[-1][0], coord[-1][1] -1))
+            # coord.append((coord[-1][0], coord[-1][1] -1))
+            if counter % 2 == 0:
+                coord.append((coord[-1][0], coord[-1][1] -1))
+            else:
+                rcoord.append((rcoord[-1][0], rcoord[-1][1] - 1))
         counter += 1
 
-unique = len(list(set(coord)))
+unique = len(list(set(coord + rcoord)))
 print(unique)
