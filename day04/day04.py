60adb90076e90409d7f08fd0cb2c849cc520ab9c
Jonny Derenge <Jderenge01@gmail.com>
2022-06-01 00:59:33 -0500

    Finished part 2

--- day04/day04.py
+++ day04/day04.py
@@ -4,7 +4,7 @@ import hashlib
 
 input = "bgvyzdsv"
 counter = 0
-checker = "00000"
+checker = "000000"
 while(1):
     ninput = input
     # Append number to end of input
@@ -12,7 +12,7 @@ while(1):
     # Hash new input
     newhash = hashlib.md5(ninput.encode())
     # # Check new hashcheck to see if hash starts with 5 leading zeros
-    if (newhash.hexdigest()[0:5] == checker):
+    if (newhash.hexdigest()[0:6] == checker):
         print(counter)
         quit
     # # If it does, print counter and break
